907e892101a31051426318e244184303950f0585
Nuibb Hasan <nuibb38@gmail.com>
2023-11-25 11:18:27 +0800

    Initial Commit

---
+++ InvestMate/InvestMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestMateApp.swift
+//  InvestMate
+//
+//  Created by Nurul Islam on 25/11/23.
+//
+
+import SwiftUI
+
+@main
+struct InvestMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
